fedaf3d5f90c4bd6fb606ffe1e0bac357609d193
Kaylany Vitoria <gamerkaylany@gmail.com>
2025-05-02 15:25:44 -0300

    Atualização

--- src/Bebida.java
+++ src/Bebida.java
@@ -1,13 +1,13 @@
 public class Bebida extends ItemCardapio {
 
-    // Construtor padrão com todos os parâmetros
+    
     public Bebida(int numItem, String nome, String descricao, double preco) {
         super(numItem, nome, descricao, preco);
     }
 
-    // Novo construtor simplificado para usar na fábrica
+   
     public Bebida(String nome, double preco) {
-        super(0, nome, "", preco); // valores padrão para os campos faltantes
+        super(0, nome, "", preco); 
     }
 
     @Override
